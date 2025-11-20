a61c25a97d66c42308294002534207cedb741184
Diego <diegoalonge13@gmail.com>
2025-11-20 21:55:24 +0100

    new change

--- src/main/java/net/diego/tutorialmod/TutorialMod.java
+++ src/main/java/net/diego/tutorialmod/TutorialMod.java
@@ -4,7 +4,7 @@ import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+// very important thing
 public class TutorialMod implements ModInitializer {
 	public static final String MOD_ID = "tutorialmod";
 
